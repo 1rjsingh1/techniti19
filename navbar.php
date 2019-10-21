4f5f03fa3577c120aad0b2b411c1de72cc6fc4bc
Ashish Kumar Yadav <yadavashishkumar97@gmail.com>
2019-10-21 23:40:23 +0530

    navbar

---
+++ navbar.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="background: black; ">
+            <div class="navbar-header" style=" margin-left: 40px;">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <img src="logo5.png" style="width: 100px; height: 50px;"> <a class="navbar-brand" href="../../../index.html" > </a></img>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse" style="background: black;">
+                <ul id="active" class="nav navbar-nav side-nav" style="background: black;">
+                   
+                    <li><a href="#Problem"><i class="fa fa-globe"></i> Event description</a></li>
+                    <li><a href="#storyline"><i class="fa fa-gamepad"></i>Team Specification</a></li>
+                    <li><a href="#rules"><i class="fa fa-list-ol"></i> Round 1</a></li>
+                    <li><a href="#testingofbridge"><i class="fa fa-universal-access"></i>Round 2</a></li>
+                    <li><a href="#testingofbridge3"><i class="fa fa-universal-access"></i>Round 3</a></li>
+                    
+                    <li><a href="#writtentest"><i class=" fa fa-clock-o"></i> Prize Money</a></li>
+ 
+                    <li><a href="https://drive.google.com/open?id=1iqwcQyieLVBrlLfIJAanRHcNnkHYFcBL" target="_blank"><i class="fa fa-file-pdf-o"></i> Rule Book</a></li>
+                    <li><a href="#contact"><i class="fa fa-phone"></i> Contacts</a></li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-left navbar-user" >
+                        <li class="dropdown user-dropdown">
+                        <a href="../../../index.html" ></a>
+                    </li> 
+                    <li class="dropdown user-dropdown">
+                        <a href="../../../index.html" ></a>
+                    </li> 
+                    <li class="dropdown user-dropdown">
+                        <a href="../../../index.html" >Home</a>
+                    </li>
+                    <li class="dropdown user-dropdown">
+                        <a href="../category.html" >Departmental Events</a>
+                    </li>
+                    <li class="dropdown user-dropdown">
+                        <a href="../category.html" >Mega Events</a>
+                    </li>
+                    <li class="dropdown user-dropdown">
+                        <a href="../category.html" >Sponsors</a>
+                    </li>
+                    <li class="dropdown user-dropdown">
+                        <a href="../category.html" >Core Team</a>
+                    </li>
+                    
+                    <li class="dropdown user-dropdown">
+                        <a href="../category.html" >Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
