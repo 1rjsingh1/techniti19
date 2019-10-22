8faa6975de9a50db5d66a5a912b04ba17c542249
Rupinderjeet Singh <1rjsingh1@gmail.com>
2019-10-22 20:15:54 +0530

    Revert "Update navbar.php"
    This reverts commit a709f5e8e354b55b3a7e5c96e7982f95577c1f12.

--- navbar.php
+++ navbar.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="../local.css" />
+    <link rel="stylesheet" type="text/css" href="../../local.css" />
     <script type="text/javascript" src="cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script src="maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
@@ -32,25 +32,25 @@
 
                 <ul class="nav navbar-nav navbar-left navbar-user" >
                         <li class="dropdown user-dropdown">
-                        <a href="../../index1.html" ></a>
+                        <a href="../../../index1.html" ></a>
                     </li>
                     <li class="dropdown user-dropdown">
-                        <a href="../../index1.html" ></a>
+                        <a href="../../../index1.html" ></a>
                     </li>
                     <li class="dropdown user-dropdown">
-                        <a href="../../index1.php" >Home</a>
+                        <a href="../../../index1.php" >Home</a>
                     </li>
                     <li class="dropdown user-dropdown">
-                        <a href="../deptcategory.html" >Departmental Events</a>
+                        <a href="../../deptcategory.html" >Departmental Events</a>
                     </li>
                     <li class="dropdown user-dropdown">
-                        <a href="../category.html" >Mega Events</a>
+                        <a href="../../category.html" >Mega Events</a>
                     </li>
                     <li class="dropdown user-dropdown">
                         <a href="#" >Sponsors</a>
                     </li>
                     <li class="dropdown user-dropdown">
-                        <a href="../../team.php" >Core Team</a>
+                        <a href="../../../team.php" >Core Team</a>
                     </li>
 
                     <li class="dropdown user-dropdown">
