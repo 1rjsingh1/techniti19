ed04069d8b83a4909f6cc1f63fcf9f94df05ca07
Rupinderjeet Singh <1rjsingh1@gmail.com>
2019-10-22 16:16:22 +0530

    Update team_register.php

--- events/departmental_events/team_register.php
+++ events/departmental_events/team_register.php
@@ -58,38 +58,38 @@ if($_POST['m7email']!=0){
 <!DOCTYPE html>
 <html lang="en">
 
-                        
+
                         <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
                             <title>Team Register</title>
                             <link rel="icon" type="image/ico" href="../../images/favlogo.ico">
-                        
+
                             <link rel="stylesheet" href="../../maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
                             <link href="../../cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" rel="stylesheet">
                             <link href="../../cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.2.0/css/mdb.min.css" rel="stylesheet">
                             <script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
                             <script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/tether/1.4.3/js/tether.min.js"></script>
-                            
+
                             <script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.2.0/js/mdb.min.js"></script>
                             <link rel="stylesheet" href="../../maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
                             <link rel="stylesheet" href="../../maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-                            <link rel="stylesheet" href="../../cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">    
+                            <link rel="stylesheet" href="../../cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                             <link rel="stylesheet" type="text/css" href="../local.css" />
                             <script type="text/javascript" src="../../cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
                             <script src="../../maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-                        
+
                         </head>
                         <body>
-                        
+
                             <div id="wrapper">
                                   <?php include('../../navbar.php'); ?>
           <div class="collapse navbar-collapse navbar-ex1-collapse" style="background: black;">
-                
-                                        
+
+
                                     </div>
                                 </nav>
-                        
+
                                 <!DOCTYPE html>
 <html>
 <style>
@@ -145,55 +145,55 @@ input[type=submit]:hover {
 >>>>>>> ea181c5f0284714b1b45c40ca1cf7ee465e3a72b
     <div style="font-size: 25px; color: black; font-family: arial;"><b>Register your team</b></div>
     <br>
-    <label class="fname">Team Name *</label>
+    <label class="fname1">Team Name *</label>
     <input type="text" id="teamname" name="teamname" placeholder="Team Name">
 
-    <label class="fname">Team Leader's Name *</label>
+    <label class="fname1">Team Leader's Name *</label>
     <input type="text" id="leadername" name="leadername" required  placeholder="Team Leader's Name">
 
-    <label class="fname">Team Leader's Email ID *</label>
+    <label class="fname1">Team Leader's Email ID *</label>
     <input type="email" id="leaderid" name="leaderid" required placeholder="Team Leader's Email ID">
 
-    <label class="fname">Member 1 Name</label>
+    <label class="fname1">Member 1 Name</label>
     <input type="text" id="m1name" name="m1name" required placeholder="Member 1">
 
-    <label class="fname">Member 1 Email ID</label>
+    <label class="fname1">Member 1 Email ID</label>
     <input type="email" id="m1email" name="m1email" required placeholder="Member 1">
 
-    <label class="fname">Member 2 Name</label>
+    <label class="fname1">Member 2 Name</label>
     <input type="text" id="m2name" name="m2name" placeholder="Member 2">
 
-    <label class="fname">Member 2 Email ID</label>
+    <label class="fname1">Member 2 Email ID</label>
     <input type="email" id="m2email" name="m2email" placeholder="Member 2">
 
-    <label class="fname">Member 3 Name</label>
+    <label class="fname1">Member 3 Name</label>
     <input type="text" id="m3name" name="m3name" placeholder="Member 3">
 
-    <label class="fname">Member 3 Email ID</label>
+    <label class="fname1">Member 3 Email ID</label>
     <input type="email" id="m3email" name="m3email" placeholder="Member 3">
 
-    <label class="fname">Member 4 Name</label>
+    <label class="fname1">Member 4 Name</label>
     <input type="text" id="m4name" name="m4name" placeholder="Member 4">
 
-    <label class="fname">Member 4 Email ID</label>
+    <label class="fname1">Member 4 Email ID</label>
     <input type="email" id="m4email" name="m4email" placeholder="Member 4">
 
-    <label class="fname">Member 5 Name</label>
+    <label class="fname1">Member 5 Name</label>
     <input type="text" id="m5name" name="m5name" placeholder="Member 5">
 
-    <label class="fname">Member 5 Email ID</label>
+    <label class="fname1">Member 5 Email ID</label>
     <input type="email" id="m5email" name="m5email" placeholder="Member 5">
 
-    <label class="fname">Member 6 Name</label>
+    <label class="fname1">Member 6 Name</label>
     <input type="text" id="m6name" name="m6name" placeholder="Member 6">
 
-    <label class="fname">Member 6 Email ID</label>
+    <label class="fname1">Member 6 Email ID</label>
     <input type="email" id="m6email" name="m6email" placeholder="Member 6">
 
-    <label class="fname">Member 7 Name</label>
+    <label class="fname1">Member 7 Name</label>
     <input type="text" id="m7name" name="m7name" placeholder="Member 7">
 
-    <label class="fname">Member 7 Email ID</label>
+    <label class="fname1">Member 7 Email ID</label>
     <input type="email" id="m7email" name="m7email" placeholder="Member 7">
 
 
@@ -216,10 +216,9 @@ input[type=submit]:hover {
                                     sessionStorage.setItem("formPath","../../form.html");
                                 }
                             </script>
-                        
+
                             <script src="../../www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
                             <script src="../../js/login.js" ></script>
                             <script src="../registration.js"></script>
                             <script src="../events.js"></script>
                         </body>
-                        
