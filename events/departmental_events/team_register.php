dc1453c8c3a52092a896c45d3840a432d7bb0f6a
Ashish Kumar Yadav <yadavashishkumar97@gmail.com>
2019-10-22 13:13:22 +0530

    team register

--- events/departmental_events/team_register.php
+++ events/departmental_events/team_register.php
@@ -6,7 +6,7 @@
                         <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <title>techNITi19 EVENT -  ALGO-RHYTHM</title>
+                            <title>Team Register</title>
                             <link rel="icon" type="image/ico" href="../../images/favlogo.ico">
                         
                             <link rel="stylesheet" href="../../maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -29,190 +29,127 @@
                             <div id="wrapper">
                                   <?php include('../../navbar.php'); ?>
           <div class="collapse navbar-collapse navbar-ex1-collapse" style="background: black;">
-                <ul id="active" class="nav navbar-nav side-nav" style="background: black;">
-                                           
-                                            <li><a href="#EventDescription"><i class="fa fa-globe"></i> Event Description:</a></li>
-                                            <li><a href="#Participation"><i class="fa fa-gamepad"></i>Participation:</a></li>
-                                            <li><a href="#ImportantDates"><i class="fa fa-list-ol"></i>Important Dates:</a></li>
-                                            <li><a href="#GeneralRules"><i class="fa fa-universal-access"></i>General Rules:</a></li>
-                                            <li><a href="#PrizeDistribution"><i class="fa fa-vcard-o"></i> Prize Distribution:</a></li>
-                                            <li><a href="#Organizers"><i class=" fa fa-clock-o"></i>Organizers:</a></li>
-                                        </ul>
+                
                                         
                                     </div>
                                 </nav>
                         
-                                <div id="page-wrapper">
-                                   <div class="row">
-                                        <div class="col-md-12">
-                                            <div class="eventHeading" style="text-align: center;">ALGO-RHYTHM</div>
-                                            
-                                            <!-- <div id="RegisterContainer">
-                                              <a class="semi-transparent-button" onclick="checkIfLogin()" id="semi-transparent-button" >REGISTER </a>
-                                            </div> -->
-                                        </div>
-                                    </div>
-                        
-                                    <br/>
-                                   
-                                    <div class="row" id="EventDescription">
-                                        <div class="col-md-12">
-                                            <div class="panel panel-primary" style="border: black" >
-                                                <div class="panel-heading" style="background: black;">
-                                                    <h3 class="panel-title"><i class="fa fa-rss"></i> Event Description:</h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <section class="feed-item">
-                                                         
-                                                        <div class="feed-item-body">
-                                                            <div class="text eventInfo" >
-                                                                   <p>The mega event organised by OpenGeest in association with Coding Club is a two-phase
-                                                                    competitive coding competition. Individuals will have to register on the techNITi’19 website in
-                                                                    order to participate.</p> <br>
-                                                                    <p>The first phase will be an online coding competition that would be organised on the
-                                                                        InterviewBit platform. Participants qualifying the phase 1 will then have to appear at NIT
-                                                                        Jalandhar, Punjab in order to appear for phase 2 that will be held onsite on the college premises.</p><br>
-                                                            </div>
-                                                        </div>
-                                                    </section>
-                        
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                        <div class="row" id="Participation">
-                                        <div class="col-md-12">
-                                            <div class="panel panel-primary" style="border: black" >
-                                                <div class="panel-heading" style="background: black;">
-                                                    <h3 class="panel-title"><i class="fa fa-rss"></i> Participation: </h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <section class="feed-item">
-                                                         
-                                                        <div class="feed-item-body">
-                                                            <div class="text eventInfo">
-                                                                    Algo-Rhythm is an individual event where participants need to qualify the first round in
-                                                                    order to appear for the onsite finale.
-                        
-                                                            </div>
-                                                        </div>
-                                                    </section>
-                        
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                        
-                                    <br />
-                                    <div class="row" id="ImportantDates">
-                        
-                                        <div class="col-md-12">
-                                            <div class="panel panel-primary" style="border: black">
-                                                <div class="panel-heading" style="background: black;">
-                                                    <h3 class="panel-title"><i class="fa fa-rss"></i>Important Dates:</h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <section class="feed-item">
-                                                         
-                                                        <div class="feed-item-body">
-                                                            <div class="text eventInfo"">
-                                                                    02 November 2019 – Online Coding Round (Phase 1)<br>
-                                                                    10 November 2019 – Onsite finale at NIT Jalandhar, Punjab (Phase 2) 
-                                                            
-                                                            </div>
-                                                        </div>
-                                                    </section>
-                        
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="row" id=">GeneralRules">
-                                     <div class="col-md-12">
-                                            <div class="panel panel-primary" style="border: black" >
-                                                <div class="panel-heading" style="background: black;">
-                                                    <h3 class="panel-title"><i class="fa fa-rss"></i> General Rules:</h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <section class="feed-item">
-                                                         
-                                                        <div class="feed-item-body">
-                                                            <div class="text eventInfo">
-                                                                    1. Both phases of the contest will be following ICPC rules.<br>
-                                                                    2. A total of 30 participants will be qualifying round 1 to appear for the onsite round.<br>
-                                                                    3. Participants should register on the techNITi’19 website.<br>
-                                                                    4. Participants must fill Google form for participation in this event.<br>
-                                                            </div>
-                                                        </div>
-                                                    </section>
-                        
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="row" id="PrizeDistribution">
-                                        <div class="col-md-12">
-                                            <div class="panel panel-primary" style="border: black" >
-                                                <div class="panel-heading" style="background: black;">
-                                                    <h3 class="panel-title"><i class="fa fa-rss"></i> Prize Distribution:</h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <section class="feed-item">
-                                                        <div class="icon pull-left">
-                                                           
-                                                        </div>
-                                                        <div class="feed-item-body">
-                                                            <div class="text eventInfo">
-                                                             <p>1st- Rs 5000</p>
-                                                             <p>2nd- Rs 3000</p>
-                                                             <p>3rd- Rs 2000</p>
-                                                            </div>
-                                                        </div>
-                                                    </section>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                        
-                                    
-                        
-                        
-                                    <div id="RegisterContainer">
-                                        <a class="semi-transparent-button" style="display: flex; justify-content: center"onclick="checkIfLogin()" id="semi-transparent-button" >REGISTER HERE</a>
-                                      </div>
+                                <!DOCTYPE html>
+<html>
+<style>
+input[type=text], input[type=email], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  font-size: 10px;
+  color: black;
+}
+
+input[type=submit] {
+  width: 100%;
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: black;
+}
+
+.register {
+  border-radius: 5px;
+  background-color: transparent;
+  padding: 50px;
+  width: auto;
+  position: relative;
+}
+.fname{
+    color: black;
+    font-size: 15px;
+}
+</style>
+<body>
 
 
-                                      <div class="row" id="Organizers">
-                                        <div class="col-md-12">
-                                            <div class="panel panel-primary" style="border: black" >
-                                                <div class="panel-heading" style="background: black;">
-                                                    <h3 class="panel-title"><i class="fa fa-rss"></i>Organizers:</h3>
-                                                </div>
-                                                <div class="panel-body">
-                                                    <section class="feed-item">
-                                                         
-                                                        <div class="feed-item-body">
-                                                            <div class="text eventInfo">
-                                                              <li>Shubham: 8727894929</li>
-                                                              <li>Pardeep:7837192997</li>
-                                                              <li>Nitish:9417787400</li>
-                                                            </div>  
-                        
-                                                        </div>
-                                                        </div>
-                                                    </section>
-                        
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                        
-                                   
-                
-                                
+<div class="register">
+  <form action="/action_page.php">
+    <div style="font-size: 25px; color: black; font-family: arial;"><b>Register your team</b></div>
+    <br>
+    <label class="fname">Team Name *</label>
+    <input type="text" id="teamname" name="teamname" placeholder="Team Name">
+
+    <label class="fname">Team Leader's Name *</label>
+    <input type="text" id="leadername" name="leadername" placeholder="Team Leader's Name">
+
+    <label class="fname">Team Leader's Email ID *</label>
+    <input type="email" id="leaderid" name="leaderid" placeholder="Team Leader's Email ID">
+
+    <label class="fname">Member 1 Name</label>
+    <input type="text" id="m1name" name="m1name" placeholder="Member 1">
+
+    <label class="fname">Member 1 Email ID</label>
+    <input type="email" id="m1email" name="m1email" placeholder="Member 1">
+
+    <label class="fname">Member 2 Name</label>
+    <input type="text" id="m2name" name="m2name" placeholder="Member 2">
+
+    <label class="fname">Member 2 Email ID</label>
+    <input type="email" id="m2email" name="m2email" placeholder="Member 2">
+
+    <label class="fname">Member 3 Name</label>
+    <input type="text" id="m3name" name="m3name" placeholder="Member 3">
+
+    <label class="fname">Member 3 Email ID</label>
+    <input type="email" id="m3email" name="m3email" placeholder="Member 3">
+
+    <label class="fname">Member 4 Name</label>
+    <input type="text" id="m4name" name="m4name" placeholder="Member 4">
+
+    <label class="fname">Member 4 Email ID</label>
+    <input type="email" id="m4email" name="m4email" placeholder="Member 4">
+
+    <label class="fname">Member 5 Name</label>
+    <input type="text" id="m5name" name="m5name" placeholder="Member 5">
+
+    <label class="fname">Member 5 Email ID</label>
+    <input type="email" id="m5email" name="m5email" placeholder="Member 5">
+
+    <label class="fname">Member 6 Name</label>
+    <input type="text" id="m6name" name="m6name" placeholder="Member 6">
+
+    <label class="fname">Member 6 Email ID</label>
+    <input type="email" id="m6email" name="m6email" placeholder="Member 6">
+
+    <label class="fname">Member 7 Name</label>
+    <input type="text" id="m7name" name="m7name" placeholder="Member 7">
+
+    <label class="fname">Member 7 Email ID</label>
+    <input type="email" id="m7email" name="m7email" placeholder="Member 7">
+
+  
+    <input style="background-color: black;" type="submit" value="Register">
+  </form>
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
                                                             
                             <div id="myModal" class="modal fade" role="dialog">
                                 <div class="modal-dialog" style="width: 90%;max-width: 90%;">
